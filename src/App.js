cce182095b26c19686cf1c64a276607f6394a022
Aftab <aftab@karigar.pk>
2020-11-18 19:15:16 +0500

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,58 +1,46 @@
 import React, {Component} from 'react';
-import MyButton from './MyComponents/MyButton';
-import MyInput from './MyComponents/MyInput';
+import Form from './MyComponents/Form';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component{
   constructor(){
     super();
+    console.log('constructor');
     this.state = {
       counter:0,
-      subOrAddValue: 1
     };
   }
 
-  handleClick = (e) => {
-    let btnClicked = e.target.innerText;
-    if(btnClicked === '+'){
-      this.setState({
-        counter: parseInt(this.state.counter) + parseInt(this.state.subOrAddValue)
-      });
-    }else{
-      this.setState({
-        counter: parseInt(this.state.counter) - parseInt(this.state.subOrAddValue)
-      });
-    }
-  }
-
-  handleChange = (e) => {
-
-    if(Number(e.target.value)){
-      this.setState({
-        subOrAddValue : parseInt(e.target.value)
-      });
-    }
-
+  componentDidMount(){
     
   }
 
+  componentDidUpdate(){
+    console.log('did update')
+  }
+  
+  SubmitData = (e) => {
+
+    const data = new FormData(e.target);
+    console.log(data);
+    fetch('http://localhost/xf2-test/admin.php?permissions/user-groups/registered.2/', {
+        method: 'POST',
+        // We convert the React state to JSON and send it as the POST body
+        body: data,
+      }).then(function(response) {
+        console.log(response)
+        return response.json();
+      });
+
+    e.preventDefault(); 
+  }
 
   render() {
     return <div className="App">
-         <h1>My First React App</h1>
-        <h2>Counter</h2>
+         <h1 className="text-center">Registration System in React</h1>
 
-      <div className="main-content">
-        
-        <div className="countingFields">
-          <MyButton text="-" buttonClick={this.handleClick} />
-          <span>{this.state.counter}</span>
-          <MyButton text="+" buttonClick={this.handleClick}  />
-        </div>
-        
-        <MyInput iVal="0" inputChange={this.handleChange} />
-      </div>
+      <Form sendData={this.SubmitData} />
       
     </div>
         
